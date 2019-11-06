49447e9371a3b1a4694ca928abe5cc1a5ee189a4
buddi56 <ynaveenk.0513@gmail.com>
2019-11-06 17:48:37 +0530

    made changes in copyrights

--- device.mk
+++ device.mk
@@ -1,5 +1,6 @@
 #
-# Copyright (C) 2018 The Android Open-Source Project
+# Copyright 2018 The Android Open Source Project
+# Copyright (C) 2019 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,6 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
 
 # Audio
 PRODUCT_PACKAGES += \
