c0f4be2956b19ec130467a059d51ddd2e8e7fef1
buddi56 <ynaveenk.0513@gmail.com>
2019-12-06 15:51:05 +0530

    changed init files to take logs while in boot animation

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -9,11 +9,17 @@ LOCAL_SRC_FILES    := etc/init.mt6771.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/init
 include $(BUILD_PREBUILT)
 
-
 include $(CLEAR_VARS)
 LOCAL_MODULE       := fstab.mt6771
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/fstab.mt6771
 LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.usb.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)/
 include $(BUILD_PREBUILT)
